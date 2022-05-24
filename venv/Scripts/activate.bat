3719af7ac54e3a3effe1e8ad4a977e5c22e55b3d
toanbui1991 <toanbui1991@gmail.com>
2022-05-24 09:47:51 +0700

    add .gitignore

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\buixu\Toan\python_projects\docker_compose\venv
+set VIRTUAL_ENV=C:\Users\LT04\toanbui1991\python\docker_compose\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
